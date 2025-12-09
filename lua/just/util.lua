37e6b9199cdbfb0e60c44e7211bf74d7fc9324d8
Steven Xu <stevenxxiu@gmail.com>
2025-12-09 21:44:27 +1100

    fix: fix errors after migrating from *JavaScript*

---
+++ lua/just/util.lua
@@ -0,0 +1,11 @@
+local _M = {}
+
+function _M:split(s, delimiter)
+  local res = {}
+  for part in string.gmatch(s, "[^"..delimiter.."]*") do
+    table.insert(res, part)
+  end
+  return res
+end
+
+return _M
